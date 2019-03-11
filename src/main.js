e51a30d7d2fc492e59178a0ba3f029d3bb409c0a
leob_o <liubobotb@126.com>
2019-03-11 20:23:23 +0800

    Add: basic dependency

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import $ from 'jquery'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min'
 
 Vue.config.productionTip = false
 
---
+++ src/views/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <button type="button" class="btn btn-default">Left</button>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
