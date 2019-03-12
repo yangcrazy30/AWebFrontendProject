c82293e95fcecd7c5fb1e3f75f46fcd051b82836
Zeo <yangcrazy30@hotmail.com>
2019-03-12 13:26:26 +0800

    add component TheImage

---
+++ src/components/TheImage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+      <div class="row">
+          <div class="col-xs-12 col=lg-12 col-md-12">
+              <img src="../assets/logo.png" alt=""  class="img-thumbnail imagecontainer">
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-lg-12 col-md-12 col-xs-12">
+              <p>{{description}}</p>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    },
+    props: {
+        description:String,
+        url:String 
+    }
+};
+</script>
+
+<style scoped>
+.container{
+    margin-bottom: 1.5em;
+}
+
+.imagecontainer{
+    margin-top: 1.5em;
+}
+</style>
+
+
