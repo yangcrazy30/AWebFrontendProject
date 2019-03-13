f34d671d3d5a3823337d0a8ce0abd0d368f54f09
Zeo <yangcrazy30@hotmail.com>
2019-03-13 09:22:54 +0800

    Detail View Update

---
+++ src/views/Detail.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="contain">
+    <aside>
+      <ul class="nav nav-pills nav-stacked">
+        <li role="presentation" class="active">
+          <a href="#">Home</a>
+        </li>
+        <li role="presentation">
+          <a href="#">Profile</a>
+        </li>
+        <li role="presentation">
+          <a href="#">Messages</a>
+        </li>
+      </ul>
+    </aside>
+
+    <section>
+      <div class="head">
+          <p>hhhhhh</p>
+          <router-view></router-view>
+      </div>
+      <footer>
+        <nav aria-label="Page navigation">
+          <ul class="pagination">
+            <li>
+              <a href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">1</a>
+            </li>
+            <li>
+              <a href="#">2</a>
+            </li>
+            <li>
+              <a href="#">3</a>
+            </li>
+            <li>
+              <a href="#">4</a>
+            </li>
+            <li>
+              <a href="#">5</a>
+            </li>
+            <li>
+              <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </section>
+    <div class="cle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+aside {
+  height: 100%;
+  background: peru;
+  float: left;
+  width: 20%;
+}
+section {
+  height: 100%;
+  background: blueviolet;
+  float: right;
+  width: 80%;
+}
+
+footer {
+  background: pink;
+  height: 10%;
+}
+
+.cle {
+  clear: both;
+}
+
+.head{
+    height: 90%;
+    background: yellow;
+}
+
+.contain{
+    height: 100%;
+}
+</style>
+
